021faccd7e10e06b9e3e3f7dfe70a0aca5b36a62
Nebiyu14 <tesfayenebiyu3@gmail.com>
2026-03-05 01:25:30 +0100

    Prepared backend for render deployement

--- server/server.js
+++ server/server.js
@@ -57,5 +57,5 @@ app.post("/create-payment-intent", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`server is running at port http://localhost:${PORT}`);
+  console.log(`server is running at port: ${PORT}`);
 });
